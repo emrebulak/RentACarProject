edcaac7de382dac691641f921796b072ab5522bc
Emre BULAK <emrbrnn@gmail.com>
2022-12-07 15:49:43 +0300

    07.12.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,14 +3,31 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
+//CarManager carManager = new CarManager(new EfCarDal());
+//Car car = new Car { Id = 3, BrandId = 2, ColorId = 3, DailyPrice = 1500, ModelYear = "2015", Description = "Temiz az yakan kiralık araba" };
+
+
+//ColorManager colorManager = new ColorManager(new EfColorDal());
+//var colors = colorManager.GetAll();
+
+//foreach (var color in colors)
+//{
+//    Console.WriteLine(color.Name);
+//}
+
 CarManager carManager = new CarManager(new EfCarDal());
-Car car = new Car { Id = 3, BrandId = 2, ColorId = 3, DailyPrice = 1500, ModelYear = "2015", Description = "Temiz az yakan kiralık araba" };
+var carDetails = carManager.GetCarDetails();
 
-
-
-carManager.Add(car);
-
-foreach (var c in carManager.GetAll())
+foreach (var carDto in carDetails)
 {
-    Console.WriteLine(c.Description);
+    Console.WriteLine("{0} - {1} - {2} - {3}", carDto.CarName, carDto.BrandName, carDto.ColorName, carDto.DailyPrice);
 }
+
+
+
+//carManager.Add(car);
+
+//foreach (var c in carManager.GetAll())
+//{
+//    Console.WriteLine(c.Description);
+//}
